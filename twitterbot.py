157416247b14ce756e92be5069af59cbd51f4057
Leonard <57763635+Snowzter@users.noreply.github.com>
2020-05-14 21:22:34 +0200

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -6,14 +6,14 @@ auth.set_access_token("ACCESS_TOKEN","ACCESS_TOKEN_SECRET")
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-search = 'Xbox'
-nrTweets = 750
+search = 'News'
+nrTweets = 500
 
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
         tweet.favorite()
         tweet.retweet()
-        time.sleep(10)
+        time.sleep(125)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
